73bda07aa283a46bad328300c82223be942fee88
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-07-21 05:50:56 -0400

    Fix outfile location. Convert fastq to fa

--- blastNt.bash
+++ blastNt.bash
@@ -2,7 +2,7 @@
 set -e
 
 for ii in data/*trimmed.fastq;do
-	outfile=$(basename $ii|sed 's/\.trimmed.fastq$//').blast
+	outfile=work/$(basename $ii|sed 's/\.trimmed.fastq$//').blast.gz
 	echo $ii -- $outfile
-	blastn -query $ii  -db ~/installs/db/nt -num_threads 20 -max_target_seqs 20 -outfmt 6|gzip > $outfile
+	awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' $ii|blastn -db ~/installs/db/nt/nt -num_threads 20 -max_target_seqs 20 -outfmt 6|gzip > $outfile
 done
