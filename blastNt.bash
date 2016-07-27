75818063acb90eb3df4c9404edbbb3a27f92b29f
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-07-27 15:58:52 -0400

    Check if file exists

--- blastNt.bash
+++ blastNt.bash
@@ -3,6 +3,10 @@ set -e
 
 for ii in data/*trimmed.fastq;do
 	outfile=work/$(basename $ii|sed 's/\.trimmed.fastq$//').blast.gz
+  if [ -e "$outfile" ];then
+    echo $outfile already exists
+    continue
+  fi
 	echo $ii -- $outfile
 	awk 'BEGIN{P=1}{if(P==1||P==2){gsub(/^[@]/,">");print}; if(P==4)P=0; P++}' $ii|blastn -db ~/installs/db/nt/nt -num_threads 20 -max_target_seqs 20 -outfmt 6|gzip > $outfile
 done
