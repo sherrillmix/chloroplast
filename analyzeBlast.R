a8a0c1d2866c6a0dc374a4d36fa7664ae7e843e7
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-07-27 16:05:32 -0400

    Pull blast hits and find taxonomy

---
+++ analyzeBlast.R
@@ -0,0 +1,28 @@
+
+source('taxaId.R')
+
+blastFiles<-list.files('work','.blast.gz$',full.names=TRUE)
+taxas<-lapply(blastFiles[1:18],function(ii){
+  message(ii)
+  outFile<-sprintf('%s_taxa.csv',sub('.blast.gz$','',ii))
+  if(file.exists(outFile)){
+    message(' Reading ',outFile)
+    taxaAssigns<-read.csv(outFile,row.names=1)
+  }else{
+    message(' Creating ',outFile)
+    x<-read.table(ii,stringsAsFactors=FALSE)
+    colnames(x)<-c('qName','tName','percIdent','alignLength','mismatch','gapOpens','qStart','qEnd','tStart','tEnd','eVal','bitScore')
+    x$accession<-sapply(strsplit(x$tName,'\\|'),'[[',4)
+    x$taxa<-accessionToTaxa(x$accession,'dump/accessionTaxa.sql')
+    x$maxBit<-ave(x$bitScore,x$qName,FUN=max)
+    x<-x[x$bitScore==x$maxBit,]
+    taxonomy<-getTaxonomy(x$taxa,taxaNodes,taxaNames)
+    taxaAssigns<-do.call(rbind,by(as.data.frame(taxonomy,stringsAsFactors=FALSE),x$qName,FUN=condenseTaxa))
+    bestScore<-x[!duplicated(x$qName),c('qName','alignLength','percIdent')]
+    rownames(bestScore)<-bestScore$qName
+    taxaAssigns<-cbind(taxaAssigns,bestScore[rownames(taxaAssigns),c('alignLength','percIdent')])
+    write.csv(taxaAssigns,outFile)
+  }
+  return(taxaAssigns)
+})
+
