0374a22cc4c0a31b3253e0039bdea38f6b8c811f
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-08-03 13:45:09 -0400

    Step through ids. Seems blocked by ncbi

--- getNcbi.R
+++ getNcbi.R
@@ -2,11 +2,17 @@ library("rentrez")
 x<-entrez_search(db="nuccore", term="rbcl",retmax=as.integer(1e7))
 #seqs<-entrez_fetch('nuccore',x$ids,rettype='fasta')
 
-#don't need this. just need to make sure retmax is an integer
-#pos<-0
-#ids<-c()
-#potentialProblem<-FALSE
-#blockSize<-10000
+allSeq<-c()
+stepSize<-10
+for(ii in seq(1,length(x$ids),stepSize)){
+  message(ii)
+  end<-max(length(x$ids),ii+stepSize-1)
+  seqs<-entrez_fetch('nuccore',x$ids[ii:end],rettype='fasta')
+  if(length(gregexpr('>',seqs)[[1]])!=end-ii+1)stop(simpleError('Missing sequences'))
+  allSeq<-c(allSeq,seqs)
+  Sys.sleep(2)
+}
+
 #while(length(tmp<-entrez_search(db="nuccore", term="rbcl",retstart=as.integer(pos),retmax=as.integer(blockSize))$ids)>0){
   #message(pos)
   #ids<-c(ids,tmp)
