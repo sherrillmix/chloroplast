820f0d5d7e59003ae101ae2bfb529830552f549d
sherrillmix <sherrillmix@users.noreply.github.com>
2016-09-20 15:00:54 -0400

    gzip rbcl.fa and zcat

--- alignRbcl.bash
+++ alignRbcl.bash
@@ -3,5 +3,5 @@ set -e
 
 makeblastdb -in rbcl/refRbcl.fa -dbtype nucl -out rbcl/rbclDb
 
-cat rbcl/rbcl.fa |parallel --block-size 100M --recstart '>' -j 50 --pipe blastn -db rbcl/rbclDb -outfmt 6|gzip>work/rbclAlign.blast.gz
+zcat rbcl/rbcl.fa.gz |parallel --block-size 100M --recstart '>' -j 50 --pipe blastn -db rbcl/rbclDb -outfmt 6|gzip>work/rbclAlign.blast.gz
 
