aee62436150c0a188f5141789a964a8c0bf4ad75
sherrillmix <sherrillmix@users.noreply.github.com>
2016-12-08 14:44:27 -0500

    Process both genes

--- cutRbcl.R
+++ cutRbcl.R
@@ -3,20 +3,19 @@ library(levenR)
 library(dnaplotr)
 ALIGN<-FALSE
 
-if(!exists('work/rbclAlign.blast.gz'))system('./alignRbcl.bash')
+if(!file.exists('work/rbclAlign.blast.gz'))system('./alignRbcl.bash')
 
-if(!exists('blast')){
-  blast<-read.blast('work/rbclAlign.blast.gz')
+for(gene in c('matk','rbcl')){
+  blast<-read.blast(sprintf('work/%sAlign.blast.gz',gene))
   blast<-blast[blast$bit==ave(blast$bit,blast$qName,FUN=max),]
   blast<-blast[blast$score>500,]
   blast<-blast[ave(blast$qName,blast$qName,FUN=length)==1,]
   rownames(blast)<-blast$qName
-}
 
-ref<-read.fa('rbcl/refRbcl.fa')
-if(!exists('seqs')){
+  capitalize<-function(x){substring(x,1,1)<-toupper(substring(x,1,1));return(x)}
+  ref<-read.fa(sprintf('%s/ref%s.fa',gene,capitalize(gene)))
   buffer<-400
-  seqs<-read.fa('rbcl/rbcl.fa.gz')
+  seqs<-read.fa(sprintf('%s/%s.fa.gz',gene,gene))
   seqs$short<-sub(' .*','',seqs$name)
   seqs<-seqs[seqs$short %in% blast$qName,]
   seqs[,c('start','end')]<-blast[seqs$short,c('qStart','qEnd')]
@@ -24,8 +23,9 @@ if(!exists('seqs')){
   #depending on substring not to care about negative start or ends past string
   seqs$trim<-substring(seqs$seq,seqs$start-buffer,seqs$end+buffer)
   seqs$trim[!seqs$isPos]<-revComp(seqs$trim[!seqs$isPos])
-  write.fa(c('ref',seqs$short),c(ref$seq,seqs$trim),'work/trimRbcl.fa')
+  write.fa(c('ref',seqs$short),c(ref$seq,seqs$trim),sprintf('work/trim%s.fa',capitalize(gene)))
 }
+
 if(ALIGN){
   if(!file.exists('work/rbclAlign.fa')){
     system('mafft --retree 1 --maxiterate 0 --thread 50 work/trimRbcl.fa>work/rbclAlign.fa')
