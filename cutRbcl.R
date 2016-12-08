00108cc11c8c9c82154297bb7440ecb2194b387f
sherrillmix <sherrillmix@users.noreply.github.com>
2016-12-08 09:00:16 -0500

    Shorten required length. Make alignment optional

--- cutRbcl.R
+++ cutRbcl.R
@@ -1,13 +1,14 @@
 library(dnar)
 library(levenR)
 library(dnaplotr)
+ALIGN<-FALSE
 
 if(!exists('work/rbclAlign.blast.gz'))system('./alignRbcl.bash')
 
 if(!exists('blast')){
   blast<-read.blast('work/rbclAlign.blast.gz')
   blast<-blast[blast$bit==ave(blast$bit,blast$qName,FUN=max),]
-  blast<-blast[blast$score>1000,]
+  blast<-blast[blast$score>500,]
   blast<-blast[ave(blast$qName,blast$qName,FUN=length)==1,]
   rownames(blast)<-blast$qName
 }
@@ -25,20 +26,22 @@ if(!exists('seqs')){
   seqs$trim[!seqs$isPos]<-revComp(seqs$trim[!seqs$isPos])
   write.fa(c('ref',seqs$short),c(ref$seq,seqs$trim),'work/trimRbcl.fa')
 }
-if(!file.exists('work/rbclAlign.fa')){
-  system('mafft --retree 1 --maxiterate 0 --thread 50 work/trimRbcl.fa>work/rbclAlign.fa')
-}
-mafft<-read.fa('work/rbclAlign.fa')
-png('test.png',height=5000,width=5000,res=500)
-  plotDNA(mafft$seq)
-dev.off()
+if(ALIGN){
+  if(!file.exists('work/rbclAlign.fa')){
+    system('mafft --retree 1 --maxiterate 0 --thread 50 work/trimRbcl.fa>work/rbclAlign.fa')
+  }
+  mafft<-read.fa('work/rbclAlign.fa')
+  png('test.png',height=5000,width=5000,res=500)
+    plotDNA(mafft$seq)
+  dev.off()
 
-lAlign<-cacheOperation('work/rbclAlign.Rdat',levenAlign,seqs$trim,ref$seq,substring1=TRUE,nThread=56)
-dists<-leven(seqs$trim,ref$seq,substring1=TRUE,nThread=56)
-png('test.png',height=5000,width=5000,res=400)
-  plotDNA(lAlign[[2]][order(dists)])
-  abline(v=range(which(!strsplit(lAlign[[1]],'')[[1]] %in% c('.','-')))+c(-.5,.5))
-dev.off()
+  lAlign<-cacheOperation('work/rbclAlign.Rdat',levenAlign,seqs$trim,ref$seq,substring1=TRUE,nThread=56)
+  dists<-leven(seqs$trim,ref$seq,substring1=TRUE,nThread=56)
+  png('test.png',height=5000,width=5000,res=400)
+    plotDNA(lAlign[[2]][order(dists)])
+    abline(v=range(which(!strsplit(lAlign[[1]],'')[[1]] %in% c('.','-')))+c(-.5,.5))
+  dev.off()
+}
 
 
 
