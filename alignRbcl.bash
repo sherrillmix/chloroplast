3bffe4c929cb318295fc12578bf1faeab762e79b
sherrillmix <sherrillmix@users.noreply.github.com>
2016-12-08 14:43:51 -0500

    Add matk blast

--- alignRbcl.bash
+++ alignRbcl.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
+#https://www.ncbi.nlm.nih.gov/gene/844754
+#https://www.ncbi.nlm.nih.gov/gene/844797
 makeblastdb -in rbcl/refRbcl.fa -dbtype nucl -out rbcl/rbclDb
+makeblastdb -in matk/refMatk.fa -dbtype nucl -out matk/matkDb
 
+#http://www.ebi.ac.uk/ena/data/search?query=rbcl
+#http://www.ebi.ac.uk/ena/data/search?query=matk
 zcat rbcl/rbcl.fa.gz |parallel --block-size 100M --recstart '>' -j 50 --pipe blastn -db rbcl/rbclDb -outfmt 6|gzip>work/rbclAlign.blast.gz
-
+zcat matk/matk.fa.gz |parallel --block-size 100M --recstart '>' -j 50 --pipe blastn -db matk/matkDb -outfmt 6|gzip>work/matkAlign.blast.gz
